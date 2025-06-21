e3586ae14f6827053773d66f05ed6a8925edfd41
ryeact-dev <rmontoya.umtc@gmail.com>
2025-06-21 11:50:24 +0800

    update to beforeload

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
-  loader: ({ context }) => {
+  beforeLoad: ({ context }) => {
     const user = context.user
 
     if (!user) {
