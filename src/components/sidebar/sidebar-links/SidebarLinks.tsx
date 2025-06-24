99ebcd6eba86d0155f10296708286c3257ef02a2
ryeact-dev <rmontoya.umtc@gmail.com>
2025-06-24 16:52:01 +0800

    Add filter for compeition links

--- src/components/sidebar/sidebar-links/SidebarLinks.tsx
+++ src/components/sidebar/sidebar-links/SidebarLinks.tsx
@@ -11,52 +11,57 @@ export default function SidebarLinks({ user }: { user: CurrentUser }) {
   return (
     <div className="flex flex-col gap-4 ">
       {/* Active Competitions */}
-      <div className=" space-y-2">
-        <h2 className="text-sm font-medium">Active Competitions</h2>
-        <Link
-          to="/settings/candidates"
-          search={{ filter: '', page: 1, sort: 'a-z', limit: 10 }}
-          className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
-        >
-          <Users2Icon size={18} />
-          <span>Candidates</span>
-        </Link>
 
-        <Link
-          to="/settings/competitions"
-          search={{ filter: '', page: 1, sort: 'a-z', limit: 10 }}
-          className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
-        >
-          <ClapperboardIcon size={18} />
-          <span>Competitions</span>
-        </Link>
-
-        {user.role === 'admin' && (
+      {/* Admin user cannot see competitions  */}
+      {/* TODO: Need to fix this to view few compeitions rather than all */}
+      {user.role !== 'admin' && (
+        <div className=" space-y-2">
+          <h2 className="text-sm font-medium">Competitions</h2>
           <Link
-            to="/settings/events"
+            to="/settings/candidates"
             search={{ filter: '', page: 1, sort: 'a-z', limit: 10 }}
             className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
           >
-            <CalendarCogIcon size={18} />
-            <span>Events</span>
+            <Users2Icon size={18} />
+            <span>Candidates</span>
           </Link>
-        )}
 
-        <Link
-          to="/settings/users"
-          search={{
-            filter: '',
-            tab: 'judges',
-            page: 1,
-            sort: 'a-z',
-            limit: 10,
-          }}
-          className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
-        >
-          <UserIcon size={18} />
-          <span>Users</span>
-        </Link>
-      </div>
+          <Link
+            to="/settings/competitions"
+            search={{ filter: '', page: 1, sort: 'a-z', limit: 10 }}
+            className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
+          >
+            <ClapperboardIcon size={18} />
+            <span>Competitions</span>
+          </Link>
+
+          {user.role === 'admin' && (
+            <Link
+              to="/settings/events"
+              search={{ filter: '', page: 1, sort: 'a-z', limit: 10 }}
+              className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
+            >
+              <CalendarCogIcon size={18} />
+              <span>Events</span>
+            </Link>
+          )}
+
+          <Link
+            to="/settings/users"
+            search={{
+              filter: '',
+              tab: 'judges',
+              page: 1,
+              sort: 'a-z',
+              limit: 10,
+            }}
+            className={`flex items-center gap-2 p-2 text-md rounded-lg hover:bg-default-100`}
+          >
+            <UserIcon size={18} />
+            <span>Users</span>
+          </Link>
+        </div>
+      )}
 
       {/* Rsults */}
       <div className=" space-y-2">
